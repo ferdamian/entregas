bdae8a004518f6862e402b3ee4099f648001f2f2
Fernando Villalba <ferdamian92@gmail.com>
2022-09-01 05:10:50 -0300

    entrega seis listo

--- entregaseis/entrega6.js
+++ entregaseis/entrega6.js
@@ -3,12 +3,13 @@ let Mis_juegos =[];
 
 //OBJETO BOARDGAME Y FUNCIONES
 class Boardgame {
-    constructor(nombre,cant_jugadores,editorial,precio,stock){
+    constructor(nombre,cant_jugadores,editorial,precio,stock,imagen){
         this.nombre = nombre;
         this.cant_jugadores = cant_jugadores;
         this.editorial = editorial;
         this.precio = precio;
         this.stock = stock;
+        this.imagen = imagen;
     }
     get_datos(){
         console.log (" <--- Juego de Mesa ", this.nombre, " --->");
@@ -25,35 +26,61 @@ class Boardgame {
 }
 
 // AGREGO ESTOS 3 ELEMENTOS EN LA LISTA PARA CORREGIR Y PROBAR SI TODO FUNCIONA SIN TENER QUE AGREGAR EN CADA MOMENTO MANUALMENTE UN NUEVO OBJETO "BOARDGAME".
-let Takenoko = new Boardgame("takenoko",4,"Devir",15000,4);
-let Carcassone = new Boardgame("carcassone",4,"Devir",12000,7);
-let Uno = new Boardgame("uno",8,"Toptoys",2000,10);
-let Tzolkin = new Boardgame("Tzolkin",5,"Devir",18000,4);
-let tickettoride = new Boardgame("Ticket to Ride",2,"Days of Wonder",20000,5);
-let clank = new Boardgame("clank",18,"Devir",21000,4);
-let truco = new Boardgame("truco",22,"N/A,",2500,6);
-Mis_juegos.push(Takenoko);
-Mis_juegos.push(Carcassone);
-Mis_juegos.push(Uno);
-Mis_juegos.push(Tzolkin);
+let takenoko = new Boardgame("Takenoko",4,"Devir",13900,4,'./img/takenoko.png');
+let carcassone = new Boardgame("Carcassone",4,"Devir",11200,7,'./img/carcassone.png');
+let uno = new Boardgame("Uno",8,"Mattel",1500,10,'./img/uno.png');
+let tzolkin = new Boardgame("Tzolkin",4,"Devir",12500,2,'./img/tzolkin.png');
+let tickettoride = new Boardgame("Ticket to Ride",5,"Days of Wonder",13650,15,'./img/tickettoride.png');
+let clank = new Boardgame("Clank",4,"Devir",14600,20,'./img/clank.png');
+let museum = new Boardgame("Museum",5,"Devir",11300,16,'./img/museum.png');
+Mis_juegos.push(takenoko);
+Mis_juegos.push(carcassone);
+Mis_juegos.push(uno);
+Mis_juegos.push(tzolkin);
 Mis_juegos.push(tickettoride);
 Mis_juegos.push(clank);
-Mis_juegos.push(truco);
+Mis_juegos.push(museum);
 
 
-
+// No se si es necesario creo una funcion. lo hice de esta manera primero para poder controlar cuando se ejecutaba
 function mostrarJuegos(){
     for(let i = 0; i < Mis_juegos.length; i ++){
+        // captar section dentro del main y crear un div para crear dentro
         let mis_productos = document.getElementById("Productos");
         let div = document.createElement("div");
-        //TEMPLATE
+        //TEMPLATE DEL DIV
         div.innerHTML = `<p>${Mis_juegos[i].nombre}</p>
-        <img class="productSize" src="../entregaseis/img/cajita.png" alt="producto">
-        <button type="button" class="btn btn-primary">Agregar al carrito</button>
+        <img class="productSize" src="${Mis_juegos[i].imagen}" alt="producto">
+        <button type="button" class="btn btn-primary marginButton">Agregar al carrito</button>
                         `
-        div.className = "bordeTemporal divSize"
-        mis_productos.append(div);
+        div.className = "bordeTemporal divSize mostrarDetalle";
+        mis_productos.append(div); // ajuntar el template al section 
+        //  EVENTO PARA MOSTRAR DETALLES
+        div.addEventListener("mouseover",function(){
+            // capturar el aside donde se van a mostrar los detalles e incorporar un div 
+            let columna = document.getElementById("detalles");
+            let divdetalles = document.createElement("div");
+            //TEMPLATE DEL DIV
+            divdetalles.innerHTML = `
+            <h2> ${Mis_juegos[i].nombre}</h2>
+            <img class="productSize" src="${Mis_juegos[i].imagen}" alt="producto">
+            <p>${Mis_juegos[i].editorial}</p>
+            <p>Jugadores:${Mis_juegos[i].cant_jugadores}</p>
+            <p>${Mis_juegos[i].precio}</p>
+            <p>Stock: ${Mis_juegos[i].stock}</p>
+                                    `
+           
+            columna.append(divdetalles);// ajuntar el template con el aside
+        })
+        // EVENTO PARA BORRAR
+        // Para cuando el usuario sale del div donde se llama al evento mouseover se reemplaze por un innerHTML vacio y "borre" la informacion anterior
+        div.addEventListener("mouseout",function(){
+            let columna = document.getElementById("detalles");
+            columna.innerHTML = "";
+            // console.log("salio");
+        })
     }
+
 }
 
 mostrarJuegos();
